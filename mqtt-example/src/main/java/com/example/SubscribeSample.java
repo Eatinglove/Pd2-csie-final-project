a3529b671fbf30d7bae34a9c8f0c27385761c9ca
unknown <djes1000303@gmail.com>
2024-06-13 16:39:55 +0800

    add user one and user two to see if two user can communicate

--- mqtt-example/src/main/java/com/example/SubscribeSample.java
+++ mqtt-example/src/main/java/com/example/SubscribeSample.java
@@ -12,9 +12,8 @@ public class SubscribeSample {
    public static void main(String[] args) {
        // MQTT 代理相關設置
        String broker = "tcp://broker.emqx.io:1883"; // MQTT 代理的地址
-       String topic="hi";
-       String topic1 = "abc"; // 訂閱的主題
-       String topic2 = "mqtt/test2"; //test topic
+       String topic="mqtt/test";
+       String topic1="hi";
        String username = "subscriber"; // 用戶名
        String password = "public"; // 密碼
        String clientid = "subscribe_client"; // 客戶端 ID
@@ -28,7 +27,7 @@ public class SubscribeSample {
            options.setUserName(username); // 設置用戶名
            options.setPassword(password.toCharArray()); // 設置密碼
            options.setConnectionTimeout(60); // 設置連接超時時間
-           options.setKeepAliveInterval(5); // 設置保持活動間隔
+           options.setKeepAliveInterval(60); // 設置保持活動間隔
 
            // 設置訂閱回調處理程序
            client.setCallback(new MqttCallback() {
@@ -42,25 +41,6 @@ public class SubscribeSample {
                    System.out.println("topic: " + topic);
                    System.out.println("Qos: " + message.getQos());
                    System.out.println("message content: " + new String(message.getPayload()));
-
-                    // 如果收到了 topic2 的訊息
-                    /*if (topic.equals(topic2)) {
-                        // 提示用戶輸入
-                        System.out.println("Do you want to unsubscribe from topic2? (y/n)");
-                        Scanner scanner = new Scanner(System.in);
-                        String input = scanner.nextLine().toLowerCase();
-                        // 如果輸入了 y，則取消訂閱 topic2 主題
-                        if (input.equals("y")) {
-                            try {
-                                client.unsubscribe(topic2);
-                                System.out.println("Unsubscribed from topic2.");
-                            } catch (MqttException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }*/
-
-
               }
 
                // 當消息傳遞完成時調用
@@ -73,11 +53,10 @@ public class SubscribeSample {
            client.connect(options);
            // 訂閱指定主題
            client.subscribe(topic,qos);
-           client.subscribe(topic1, qos);
-           client.subscribe(topic2, qos);
+           client.subscribe(topic1,qos);
 
            // 等待一段時間，接收消息，然後斷開連接
-           Thread.sleep(60000); // 等待 30 秒鐘
+           Thread.sleep(60000); // 等待 60 秒鐘
            client.disconnect(); // 斷開連接
            System.out.println("Disconnected from broker.");
       } catch (Exception e) {
